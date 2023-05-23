6a67b9749db479720bc464bb006dedb6950690bc
Menz, Anika <anika.menz@study.thws.de>
2023-05-23 11:53:41 +0200

    added searchbar

--- src/views/AddPage.vue
+++ src/views/AddPage.vue
@@ -1,3 +1,5 @@
+
+
 <template>
   <ion-page>
     <ion-header>
@@ -15,28 +17,105 @@
 
       <ExploreContainer name="Add page" />
 
-      test blabla
-
-      <div> 
-        <ion-button > 
         
-          Lucas Button
-        </ion-button>
-        
-<button   <router-link to="/tabs/logbuch" tag="button"></router-link> Test</button>
-      </div>
-      <Counter/>
+      <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M256 176v160M336 256H176"/></svg>
+  
+      <h1> Produkt hinzufügen</h1>
 
-      <h1> Hier kommt der Testbutton</h1>
       
-      <counter/>
+
+    <div class = "Input"> 
+
+      <ion-item>
+    <ion-input
+      label="Input with clear button"
+      labelPlacement="stacked"
+      clearInput="true"
+      placeholder="Enter text to see clear button"
+      value="Default value"
+      class="customInput"
+    >
+    </ion-input>
+  </ion-item>
+
+
+
+    </div>
+     
+      
+      
+      
+    
+     
+     
     </ion-content>
   </ion-page>
+
+  
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+    import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon} from '@ionic/vue';
+    import { add, book, ellipse, home, person, square, triangle, addCircleOutline, serverOutline, star } from 'ionicons/icons';
+    import {defineComponent} from 'vue';
+
+  //  import Counter from './Counter.vue';
 
 
+  //   export default defineComponent({
+  //   components:{
+  //     Counter
+  //  },
+  
+  // });
 
 </script>
+ 
+
+
+
+<style>
+
+@import url('https://fonts.cdnfonts.com/css/inter');
+
+
+svg{
+
+  color: #07C258;
+  position: fixed;
+    width: 100%;
+  height: 50px;
+  left: 266px;
+  top: 105px;
+
+}
+
+h1{
+
+  position: absolute;
+width: 185px;
+height: 88px;
+left: 63px;
+top: 74px;
+
+font-family: 'Inter';
+font-style: normal;
+font-weight: 700;
+font-size: 36px;
+line-height: 44px;
+text-align: right;
+letter-spacing: -0.05em;
+
+color: #323232;
+
+
+}
+
+
+
+
+
+
+
+</style>
---
+++ src/views/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2><button v-on:click="decrease">decrease</button> </h2>
+    <h3><button v-on:click="increment">increment</button> </h3>
+    
+    
+    <p>{{ count }}</p>
+  </div>
+</template>
+<script lang="ts">
+  import { DEPRECATED_ALIASES } from '@babel/types';
+import { defineComponent} from 'vue';
+  export default defineComponent({
+   components:{},
+   data(){
+     return{
+     count:0
+     }
+    },
+   methods:{
+      increment() {
+       this.count++;
+      },
+
+      decrease(){
+        this.count--;
+      }
+    }
+  });
+
+</script>
