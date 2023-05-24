ec4ab28d5bb0f6345b7af25850dd204394130edb
k53299 <maximilian.groll@study.thws.de>
2023-05-24 14:17:21 +0200

    test123

--- src/Logik/AddProduct.vue
+++ src/Logik/AddProduct.vue
@@ -71,59 +71,59 @@
       saveProducts() {
         window.localStorage.setItem('products', JSON.stringify(this.items));
       },
-  addProduct() {
-  const product = {
-  id: Date.now(),
-  name: this.newProduct.name,
-  calories: this.newProduct.calories,
-  carbs: this.newProduct.carbs,
-  protein: this.newProduct.protein,
-  fat: this.newProduct.fat
-  };
-  this.items.push(product);
-  this.saveProducts();
-  this.resetForm();
-  },
-  updateProduct() {
-  const updatedProduct = {
-  id: this.newProduct.id,
-  name: this.newProduct.name,
-  calories: this.newProduct.calories,
-  carbs: this.newProduct.carbs,
-  protein: this.newProduct.protein,
-  fat: this.newProduct.fat
-  };
-  this.items.splice(this.editProductIndex, 1, updatedProduct);
-  this.saveProducts();
-  this.cancelEdit();
-  },
-  deleteProduct(product) {
-  const index = this.items.indexOf(product);
-  if (index !== -1) {
-  this.items.splice(index, 1);
-  this.saveProducts();
-  }
-  },
-  editProduct(product) {
-  this.editMode = true;
-  this.editProductIndex = this.items.indexOf(product);
-  this.newProduct = { ...product };
-  },
-  cancelEdit() {
-  this.editMode = false;
-  this.editProductIndex = null;
-  this.resetForm();
-  },
-  resetForm() {
-  this.newProduct = {
-  id: null,
-  name: '',
-  calories: 0,
-  carbs: 0,
-  protein: 0,
-  fat: 0
-  };
-  }
-  }
+      addProduct() {
+      const product = {
+      id: Date.now(),
+      name: this.newProduct.name,
+      calories: this.newProduct.calories,
+      carbs: this.newProduct.carbs,
+      protein: this.newProduct.protein,
+      fat: this.newProduct.fat
+      };
+      this.items.push(product);
+      this.saveProducts();
+      this.resetForm();
+      },
+      updateProduct() {
+      const updatedProduct = {
+      id: this.newProduct.id,
+      name: this.newProduct.name,
+      calories: this.newProduct.calories,
+      carbs: this.newProduct.carbs,
+      protein: this.newProduct.protein,
+      fat: this.newProduct.fat
+      };
+      this.items.splice(this.editProductIndex, 1, updatedProduct);
+      this.saveProducts();
+      this.cancelEdit();
+      },
+      deleteProduct(product) {
+      const index = this.items.indexOf(product);
+      if (index !== -1) {
+      this.items.splice(index, 1);
+      this.saveProducts();
+      }
+      },
+      editProduct(product) {
+      this.editMode = true;
+      this.editProductIndex = this.items.indexOf(product);
+      this.newProduct = { ...product };
+      },
+      cancelEdit() {
+      this.editMode = false;
+      this.editProductIndex = null;
+      this.resetForm();
+      },
+      resetForm() {
+      this.newProduct = {
+      id: null,
+      name: '',
+      calories: 0,
+      carbs: 0,
+      protein: 0,
+      fat: 0
+      };
+    }
+    }
   };
   </script>
