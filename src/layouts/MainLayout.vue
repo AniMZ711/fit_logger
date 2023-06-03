42263522bef1028c0becc65486931d681c53b171
Menz, Anika <anika.menz@study.thws.de>
2023-06-03 11:37:37 +0200

    Font sizes adapt to screen size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,10 +7,10 @@
 
 
     <q-footer reveal elevated class="bg-grey-8 text-white">
-      <q-tabs align="justify" :breakpoint="0">
+      <q-tabs align="justify" :breakpoint="xs"> <!-- Tabs wachsen und schrumpfen automatisch mit-->
         <q-route-tab icon="home" to="/dashboard" label="Home" />
         <q-route-tab icon="menu_book" to="/logbuch" label="Logbuch" />
-        <q-page-sticky position="bottom" :offset="[18, 18]" class="fabbutton">
+        <q-page-sticky position="bottom" :offset="[18, 18]" id="middleAddButton">
           <q-btn round color="green" icon="add_circle" size="28px" padding="5px" to="/addproduct" />
         </q-page-sticky>
         <q-route-tab icon="storage" to="/produkteinpflegen" label="Produkte" />
@@ -28,11 +28,7 @@
 </script>
 
 <style>
-q-page {
-  overflow: hidden;
-}
-
-.fabbutton {
+#middleAddButton {
   position: fixed;
 
   bottom: -45px;
@@ -43,65 +39,35 @@ q-page {
   z-index: 3000;
 }
 
-.toolbar {
-  margin-left: auto;
-  margin-right: auto;
-}
 
-/* For desktop: */
-.col-1 {
-  width: 8.33%;
-}
+/* media querries erstmal nur für 3 verschiedene Größen, später kommen evtl noch mehr dazu */
 
-.col-2 {
-  width: 16.66%;
-}
+@media (max-width: 576px) {
+  /* für kleine Bildschirme (Handys,..) */
 
-.col-3 {
-  width: 25%;
-}
+  body {
 
-.col-4 {
-  width: 33.33%;
-}
-
-.col-5 {
-  width: 41.66%;
-}
-
-.col-6 {
-  width: 50%;
-}
-
-.col-7 {
-  width: 58.33%;
-}
-
-.col-8 {
-  width: 66.66%;
-}
-
-.col-9 {
-  width: 75%;
-}
-
-.col-10 {
-  width: 83.33%;
-}
-
-.col-11 {
-  width: 91.66%;
-}
-
-.col-12 {
-  width: 100%;
-}
-
-@media only screen and (max-width: 768px) {
-
-  /* For mobile phones: */
-  [class*="col-"] {
-    width: 100%;
+    font-size: 14px;
   }
+}
+
+@media (min-width: 768px) {
+
+  body {
+
+    font-size: 16px;
+  }
+
+  /* mittlere Bildschirme */
+}
+
+@media (min-width: 992px) {
+
+  body {
+
+    font-size: 18px;
+  }
+
+  /* große Bilschirme */
 }
 </style>
