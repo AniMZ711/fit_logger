97e372775a9c83595aee6ad9ad2bc0a9038ef7b2
Luca Lorenz <luca.lorenz@student.fhws.de>
2023-06-14 14:58:00 +0200

    Optische Anpassung Produkt Komponieren Seite

--- src/pages/EssenKomponierenPage.vue
+++ src/pages/EssenKomponierenPage.vue
@@ -8,51 +8,94 @@
   </q-header>
 
   <div>
-    <h2>Mahlzeit-Komponierung</h2>
-    <div>
-      <label for="mealNameInput">Name der Mahlzeit:</label>
-      <input type="text" id="mealNameInput" v-model="mealName" />
+    <div class="NameEingeben">
+      <q-input
+        color="green"
+        label="Name der neuen Mahlzeit"
+        type="text"
+        id="mealNameInput"
+        v-model="mealName"
+      />
     </div>
-    
-    <div>
-      <label for="searchInput">Produktsuche:</label>
-      <input type="text" id="searchInput" v-model="searchTerm" @input="searchProducts" />
+
+    <div class="Suchleiste">
+      <q-input
+        color="green"
+        filled
+        v-model="searchQuery"
+        label="Suche die Produkte, die du kopomnieren möchtest ... "
+        type="text"
+      >
+        <template v-slot:after>
+          <q-btn
+            rounded
+            size="lg"
+            color="green"
+            icon="search"
+            @click="searchProducts"
+          ></q-btn>
+        </template>
+      </q-input>
     </div>
 
     <div v-if="searchResults.length > 0">
-      <h3>Ergebnisse:</h3>
-      <ul>
-        <li v-for="product in searchResults" :key="product.id">
-          <label>
-            <input type="checkbox" v-model="selectedProducts[product.id]" />
-            {{ product.name }} - {{ product.calories }} Kalorien, {{ product.carbs }} Kohlenhydrate, {{ product.protein }} Protein, {{ product.fat }} Fett
-          </label>
-          <select v-model="productQuantities[product.id]">
-            <option value="1/4">1/4</option>
-            <option value="1/2">1/2</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-          </select>
-        </li>
-      </ul>
+      <div class="Ausgabebereich2">
+        <h6>Ergebnisse:</h6>
+        <q-scroll-area style="height: 250px; max-width: 90%">
+          <ul>
+            <li v-for="product in searchResults" :key="product.id">
+              <label>
+                <input type="checkbox" v-model="selectedProducts[product.id]" />
+                {{ product.name }} - {{ product.calories }} Kalorien,
+                {{ product.carbs }} Kohlenhydrate,
+                {{ product.protein }} Protein, {{ product.fat }} Fett
+              </label>
+              <select v-model="productQuantities[product.id]">
+                <option value="1/4">1/4</option>
+                <option value="1/2">1/2</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+              </select>
+            </li>
+          </ul>
+        </q-scroll-area>
+      </div>
     </div>
 
     <div>
-      <h3>Ausgewählte Produkte:</h3>
+      <h5>Summe:</h5>
       <ul>
         <li v-for="product in selectedProductsList" :key="product.id">
-          {{ product.name }} - {{ formatQuantity(productQuantities[product.id]) }} - {{ calculateTotal(product) }} Gesamt Kalorien, {{ calculateTotal(product, 'carbs') }} Gesamt Kohlenhydrate, {{ calculateTotal(product, 'protein') }} Gesamt Protein, {{ calculateTotal(product, 'fat') }} Gesamt Fett
+          {{ product.name }} -
+          {{ formatQuantity(productQuantities[product.id]) }} -
+          {{ calculateTotal(product) }} Gesamt Kalorien,
+          {{ calculateTotal(product, "carbs") }} Gesamt Kohlenhydrate,
+          {{ calculateTotal(product, "protein") }} Gesamt Protein,
+          {{ calculateTotal(product, "fat") }} Gesamt Fett
         </li>
       </ul>
-      <p>Gesamtsumme: {{ calculateMealTotal() }} Gesamt Kalorien, {{ calculateMealTotal('carbs') }} Gesamt Kohlenhydrate, {{ calculateMealTotal('protein') }} Gesamt Protein, {{ calculateMealTotal('fat') }} Gesamt Fett</p>
-      <button @click="saveMeal">Mahlzeit speichern</button>
+      <p>
+        Gesamtsumme: {{ calculateMealTotal() }} Gesamt Kalorien,
+        {{ calculateMealTotal("carbs") }} Gesamt Kohlenhydrate,
+        {{ calculateMealTotal("protein") }} Gesamt Protein,
+        {{ calculateMealTotal("fat") }} Gesamt Fett
+      </p>
+      <div class="save-button">
+        <q-btn
+          @click="saveMeal"
+          icon="save"
+          rounded
+          color="green"
+          size="lg"
+        ></q-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -74,7 +117,9 @@ export default defineComponent({
   },
   computed: {
     selectedProductsList() {
-      return this.products.filter((product) => this.selectedProducts[product.id]);
+      return this.products.filter(
+        (product) => this.selectedProducts[product.id]
+      );
     },
   },
   created() {
@@ -85,48 +130,48 @@ export default defineComponent({
       const savedProducts = localStorage.getItem("products");
       if (savedProducts) {
         this.products = JSON.parse(savedProducts);
-}
-this.selectedProducts.forEach((product) => {
-this.$set(this.productQuantities, product.id, "1");
-});
-},
-searchProducts() {
-this.searchResults = this.products.filter((product) =>
-product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
-);
-},
-formatQuantity(quantity) {
-if (quantity === "1/4") {
-return "1/4";
-} else if (quantity === "1/2") {
-return "1/2";
-} else {
-return quantity;
-}
-},
-calculateTotal(product, type) {
-const quantity = this.productQuantities[product.id];
-if (quantity === "1/4") {
-return product[type] * 0.25;
-} else if (quantity === "1/2") {
-return product[type] * 0.5;
-} else {
-return product[type] * parseFloat(quantity);
-}
-},
-calculateMealTotal(type) {
-let total = 0;
-this.selectedProductsList.forEach((product) => {
-const quantity = this.productQuantities[product.id];
-if (type === undefined) {
-total += product.calories * parseFloat(quantity);
-} else {
-total += product[type] * parseFloat(quantity);
-}
-});
-return total;
-},
-saveMeal() {
+      }
+      this.selectedProducts.forEach((product) => {
+        this.$set(this.productQuantities, product.id, "1");
+      });
+    },
+    searchProducts() {
+      this.searchResults = this.products.filter((product) =>
+        product.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    },
+    formatQuantity(quantity) {
+      if (quantity === "1/4") {
+        return "1/4";
+      } else if (quantity === "1/2") {
+        return "1/2";
+      } else {
+        return quantity;
+      }
+    },
+    calculateTotal(product, type) {
+      const quantity = this.productQuantities[product.id];
+      if (quantity === "1/4") {
+        return product[type] * 0.25;
+      } else if (quantity === "1/2") {
+        return product[type] * 0.5;
+      } else {
+        return product[type] * parseFloat(quantity);
+      }
+    },
+    calculateMealTotal(type) {
+      let total = 0;
+      this.selectedProductsList.forEach((product) => {
+        const quantity = this.productQuantities[product.id];
+        if (type === undefined) {
+          total += product.calories * parseFloat(quantity);
+        } else {
+          total += product[type] * parseFloat(quantity);
+        }
+      });
+      return total;
+    },
+    saveMeal() {
       const existingProducts = window.localStorage.getItem("products");
       const products = existingProducts ? JSON.parse(existingProducts) : [];
 
@@ -153,6 +198,28 @@ saveMeal() {
         fat: 0,
       };
     },
-},
+  },
 });
 </script>
+
+<style>
+.NameEingeben {
+  margin-top: 2em;
+  width: 90%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+.Suchleiste {
+  margin-top: 2em;
+  width: 90%;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+.save-button {
+  text-align: center;
+  margin-bottom: 5em;
+}
+.Ausgabebereich2 {
+  text-align: center;
+}
+</style>
