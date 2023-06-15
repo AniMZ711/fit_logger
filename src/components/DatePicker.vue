299bf1ed6095282939a389659ff42e90c5497a52
Menz, Anika <anika.menz@study.thws.de>
2023-06-15 14:57:33 +0200

    Fixed Calender Poistioning Date Picker

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -7,7 +7,26 @@
     <q-btn flat icon="arrow_forward" color="green" @click="increaseDate">
     </q-btn>
 
-    <q-btn color="green" round name="event" icon="event" class="cursor-pointer">
+
+    <q-btn color="green" round icon="event" @click="popupDate = true">
+
+    </q-btn>
+
+
+
+    <q-dialog large persistent v-model="popupDate">
+
+      <q-date v-model="date" today-btn>
+        <q-btn v-close-popup label="Close" color="primary" flat></q-btn>
+        <q-btn v-close-popup label="Speichern" color="primary" flat @click="saveDate"></q-btn>
+      </q-date>
+
+
+    </q-dialog>
+
+
+
+    <!-- <q-btn color="green" round name="event" icon="event" class="cursor-pointer">
       <q-popup-proxy
         @before-show="updateProxy"
         cover
@@ -25,7 +44,7 @@
           ></q-btn>
         </q-date>
       </q-popup-proxy>
-    </q-btn>
+    </q-btn> -->
   </div>
 </template>
 
@@ -37,6 +56,7 @@ export default defineComponent({
     const date = ref(new Date()); // heutiges Datum
     return {
       date, //
+      popupDate: ref(false)
     };
   },
   created() {
@@ -82,7 +102,10 @@ export default defineComponent({
       console.log("done");
     },
 
-    updateProxy() {},
+    updateProxy() {
+
+
+    },
   },
 });
 </script>
